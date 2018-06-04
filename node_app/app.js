1c64c989f230e84e54634498fc117c8ef5c176ad
Zayne  Berndt <zayneberndt@Zaynes-MacBook-Pro-2.local>
2018-06-04 17:34:39 +0100

    add date pickers

--- node_app/app.js
+++ node_app/app.js
@@ -1,15 +1,22 @@
 var http = require('http');
 var mysql = require('mysql');
 var fs = require('fs');
+var calendar = require('calendar-month-array');
+var sprintf = require("sprintf-js").sprintf;
 var express = require('express');
-
 var app = express();
+
+var weeks = calendar(new Date(2018, 9), { 
+	
+})
+
+
+app.get('/api/getDates', function(req, res, next){
+	res.send(weeks); 
+})
 
 app.use(express.static('../public'))
 
-
-
-
 app.listen(8080, function() {
-	console.log("Running Express")
+    console.log("Running Express")
 });
