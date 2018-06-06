d9b1e5da7ca9d25b3eed033494d5759f5908d535
Zayne  Berndt <zayneberndt@Zaynes-MacBook-Pro-2.local>
2018-06-06 18:27:15 +0100

    working calendar

--- node_app/app.js
+++ node_app/app.js
@@ -4,18 +4,52 @@ var fs = require('fs');
 var calendar = require('calendar-month-array');
 var sprintf = require("sprintf-js").sprintf;
 var express = require('express');
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+
 var app = express();
 
-var weeks = calendar(new Date(2018, 9), { 
-	
-})
-
+app.use(express.static('../public'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
 
 app.get('/api/getDates', function(req, res, next){
 	res.send(weeks); 
 })
 
-app.use(express.static('../public'))
+app.post('/api/sendMail', function(req, res, next){
+	console.log(req.body) 
+	// transporter.sendMail(mailOptions, function(error, info) {
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //     }
+    // });
+})
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: '******@gmail.com',
+    pass: '*****'
+  }
+});
+
+
+var mailOptions = {
+  from: '*******@gmail.com',
+  to: 'berndt.zayne@gmail.com',
+  subject: 'Sending Email using Node.js',
+  text: "You have sent a succesful email"
+}
+
+
+
+
+
 
 app.listen(8080, function() {
     console.log("Running Express")
