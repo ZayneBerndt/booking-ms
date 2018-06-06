6458ef7fe9c01fcf8780a8f558922175d30705b2
Zayne  Berndt <zayneberndt@Zaynes-MacBook-Pro-2.local>
2018-06-06 18:27:55 +0100

    working calendar

--- public/selector.js
+++ public/selector.js
@@ -1,82 +1,167 @@
 var wsurl = 'http://localhost:8080';
-var d = new Date();
-var year = d.getFullYear();
-var daysInMonth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-var monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 var today = new Date();
 var currentDate;
-today.setDate(today.getDate());
-currentDate = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
+	today.setDate(today.getDate());
+	currentDate = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
 
-document.getElementById('dateSpot').innerHTML = currentDate;
+// define variables
+var nativePicker = document.querySelector('.nativeDatePicker');
+var yearSelect = document.querySelector('#year');
+var monthSelect = document.querySelector('#month');
+var fromDaySelect = document.querySelector('#fromDay');
+var toDaySelect = document.querySelector('#toDay');
+document.getElementById('fromDay').min = currentDate;
 
-function selectedFrom() {
-    var fromDate = document.getElementById('dateFrom').value;
-    document.getElementById('displayFrom').innerHTML = fromDate;
-    if (currentDate == fromDate ) {
-		console.log("Hey there you selected todays date");
-	}
+toDaySelect.disabled = true;
+
+fromDaySelect.onchange = function fromDaySelected() {
+  if (fromDaySelect.value < currentDate) {
+  } else {
+      toDaySelect.disabled = false;
+      document.getElementById('toDay').min = fromDaySelect.value;
+  }
 }
 
-var toDate = document.getElementById('dateTo').value;
-document.getElementById('displayTo').innerHTML = toDate;
-
-document.getElementById('date').innerHTML = monthArr[0];
-
-var i = 0;
-function nextMonth() {
-    if (i == monthArr.length - 1)
-        i = 0
-    else
-        i++;
-    document.getElementById("date").innerHTML = monthArr[i];
+toDaySelect.onchange = function toDaySelected() {
+  console.log("You have booked " + fromDaySelect.value + " to the " + toDaySelect.value);
 }
-function prevMonth() {
-    if (i == 0)
-        i = month.length - 1;
-    else
-        i--;
-    document.getElementById("date").innerHTML = monthArr[i];
+
+function postData(url, data) {
+  // Default options are marked with *
+  return fetch(url, {
+    body: JSON.stringify(data), // must match 'Content-Type' header
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+  })
+  .then(response => response.json()) // parses response to JSON
 }
 
 
 
-// function getDaysInMonth(month, year) {
-//          // Since no month has fewer than 28 days
-//          var date = new Date(year, month, 1);
-//          var days = [];
-//          console.log('month', month, 'date.getMonth()', date.getMonth())
-//          while (date.getMonth() === month) {
-//             days.push(new Date(date));
-//             date.setDate(date.getDate() + 1);
-//          }
-//          return days;
+postData(wsurl + '/api/sendMail', {answer: 42, name: 'hgjghjgh'})
+ .then(data => console.log(data)) 
+ .catch(error => console.error(error))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // test whether a new date input falls back to a text input or not
+// var test = document.createElement('input');
+// test.type = 'date';
+// // if it does, run the code inside the if() {} block
+// if(test.type === 'text') {
+//   // hide the native picker and show the fallback
+//   nativePicker.style.display = 'none';
+//   // populate the days and years dynamically
+//   // (the months are always the same, therefore hardcoded)
+//   populateDays(monthSelect.value);
+//   populateYears();
+// }
+
+// function populateDays(month) {
+//   // delete the current set of <option> elements out of the
+//   // day <select>, ready for the next set to be injected
+//   while(daySelect.firstChild){
+//     daySelect.removeChild(daySelect.firstChild);
+//   }
+//   // Create variable to hold new number of days to inject
+//   var dayNum;
+//   // 31 or 30 days?
+//   if(month === 'January' || month === 'March' || month === 'May' || month === 'July' || month === 'August' || month === 'October' || month === 'December') {
+//     dayNum = 31;
+//   } else if(month === 'April' || month === 'June' || month === 'September' || month === 'November') {
+//     dayNum = 30;
+//   } else {
+//   // If month is February, calculate whether it is a leap year or not
+//     var year = yearSelect.value;
+//     (year - 2016) % 4 === 0 ? dayNum = 29 : dayNum = 28;
+//   }
+//   // inject the right number of new <option> elements into the day <select>
+//   for(i = 1; i <= dayNum; i++) {
+//     var option = document.createElement('option');
+//     option.textContent = i;
+//     daySelect.appendChild(option);
+//   }
+//   // if previous day has already been set, set daySelect's value
+//   // to that day, to avoid the day jumping back to 1 when you
+//   // change the year
+//   if(previousDay) {
+//     daySelect.value = previousDay;
+//     // If the previous day was set to a high number, say 31, and then
+//     // you chose a month with less total days in it (e.g. February),
+//     // this part of the code ensures that the highest day available
+//     // is selected, rather than showing a blank daySelect
+//     if(daySelect.value === "") {
+//       daySelect.value = previousDay - 1;
 //     }
-        
-//     console.log(getDaysInMonth(5, 2018))
-// var Obj = JSON.stringify(month); 
-
-
-
-
-// fetch('./api/getDates')
-//   .then(
-//     function(response) {
-//       if (response.status !== 200) {
-//         console.log('Looks like there was a problem. Status Code: ' + response.status);
-//         return;
-//       }
-//       // Examine the text in the response
-//       response.json().then(function(weeks) {	
-   	
-       
-    
-//       });
+//     if(daySelect.value === "") {
+//       daySelect.value = previousDay - 2;
 //     }
-//   )
-//   .catch(function(err) {
-//     console.log('Fetch Error :-S', err);
-//   });
+//     if(daySelect.value === "") {
+//       daySelect.value = previousDay - 3;
+//     }
+//   }
+// }
+
+// function populateYears() {
+//   // get this year as a number
+//   var date = new Date();
+//   var year = date.getFullYear();
+
+//   // Make this year, and the 100 years before it available in the year <select>
+//   for(var i = 0; i <= 100; i++) {
+//     var option = document.createElement('option');
+//     option.textContent = year-i;
+//     yearSelect.appendChild(option);
+//   }
+// }
+
+// // when the month or year <select> values are changed, rerun populateDays()
+// // in case the change affected the number of available days
+// yearSelect.onchange = function() {
+//   populateDays(monthSelect.value);
+// }
+
+// monthSelect.onchange = function() {
+//   populateDays(monthSelect.value);
+// }
+
+// //preserve day selection
+// var previousDay;
+
+// // update what day has been set to previously
+// // see end of populateDays() for usage
+// daySelect.onchange = function() {
+//   previousDay = daySelect.value;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
